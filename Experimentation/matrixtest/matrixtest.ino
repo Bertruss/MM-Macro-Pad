3342a027c17f7e0e649dffda7b7e5147f48a868b
Bertruss <mikehautman@gmail.com>
2016-11-15 01:19:51 +0000

    Cleaned up code, implemented debouncing. Reached stopping point, ready to merge back to master.

--- Experimentation/matrixtest/matrixtest.ino
+++ Experimentation/matrixtest/matrixtest.ino
@@ -1,21 +1,39 @@
 //9 button matrix with cooldown and debounce
 
 // pin assignment
+//columns and rows numbered 0, 1, 2
 const int col[3] PROGMEM = {1, 2, 3};
 const int row[3] PROGMEM = {9, 10, 11};
+
+// You can map the keys however you wish, 
+// but this is how it is  configured by default
+//
+//       the number on each key in the map
+//       below marks its index in the buffer
+//       as well as it's signifier in the
+//       operation queue
+//
+//               0    1    2      
+//            |----|----|----|
+//          0 | 0  | 1  | 2  |
+//            |----|----|----|
+//          1 | 3  | 4  | 5  |
+//            |----|----|----|
+//          2 | 6  | 7  | 8  |
+//            |----|----|----|
+
 
 //arrays for button debouncing and cooldown
 int button_buff[9];
 int button_lastState[9] = {0,0,0,0,0,0,0,0,0};
 unsigned long debounce_timer[9];
-unsigned long cooldown_timer[9];
 
-//how long a button needs to be depressed in order to be read
-const unsigned long delayt = 50;
-const unsigned cooldown = 300;
+//how long a button needs to be depressed in order to be read, in ms
+const unsigned long delayt = 20;
 
+//given col and row, returns button address
 int buttonaddr(int rownum, int colnum){
-  return (3 * rownum) + (colnum + 1);
+  return (3 * rownum) + (colnum);
   }
 
 //debouncing pin reader
@@ -25,17 +43,10 @@ void debounce(int rownum, int colnum){
   
   if(readPin != button_lastState[address]){
     debounce_timer[address] = millis();
-     //when going from high to low, reset cooldown marker;
-      if(readPin == LOW){ 
-        cooldown_timer[address] = millis();
-      }
     }
     
   if((millis() - debounce_timer[address]) > delayt && readPin != button_buff[address]){
-      //if its been more than the cooldown timer since last transitioned from high to low, allow button to be set to high 
-      if(((millis() - cooldown_timer[address]) > cooldown && readPin == HIGH) || readPin == LOW){
-        button_buff[address] = readPin;
-      } 
+      button_buff[address] = readPin;
     }
   button_lastState[address] = readPin;
   }
@@ -55,33 +66,21 @@ void rowSet(int rowIn) {
    }
 
 
-//checks every button in the matrix, runs debounce on it 
+//checks every button in the matrix, runs "debounce" on it 
 void scan(){
     //numbered by array index 0 to two. 
-    int rownum;
-    int colnum;
-    //reads every key, stores in the button buffer array.
-    for(rownum = 0; rownum < 3; rownum = rownum + 1){//checks each row
+    int rownum, colnum;
+    for(rownum = 0; rownum < 3; rownum = rownum + 1){//increments rownum and sets the row with that number to HIGH
       rowSet(rownum);
-      for(colnum = 0; colnum < 3; colnum = colnum + 1){
+      for(colnum = 0; colnum < 3; colnum = colnum + 1){//increments colnum and runs "debounce" on that column
         debounce(rownum, colnum);
       }
     }
   }
 
-void bufferRead(){ //only reads one key at a time, but this is just debug
-    int cnt;
-    for(cnt = 0; cnt < 8; cnt = cnt + 1){
-        if(button_buff[cnt] == HIGH){
-            int out = cnt + 1;
-            Keyboard.print(out);
-          }    
-      }
-   }
-
 void setup() {
   int cnt;
-    //button matrix instantiation
+  //button matrix instantiation
   for (cnt = 0; cnt < 3; cnt = cnt + 1){ //instantiates every pin in row[] to OUTPUT and in col[] to INPUT_PULLUP
       pinMode(row[cnt],OUTPUT);
       pinMode(col[cnt],INPUT_PULLUP);
@@ -90,7 +89,6 @@ void setup() {
 
 void loop() {
   scan();
-  bufferRead();
 }
 
 
