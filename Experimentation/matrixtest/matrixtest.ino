a391f3be9c5041260c553cf873e0410f87b34296
Bertruss <mikehautman@gmail.com>
2016-11-26 19:34:43 +0000

    fully implemented all the intended features, code might do with some cleaning, though. expect next commit to be code legibility and efficiency related.

--- Experimentation/matrixtest/matrixtest.ino
+++ Experimentation/matrixtest/matrixtest.ino
@@ -1,4 +1,5 @@
 #include <LCqueue.h>
+#include <usb_keyboard.h>
 
 //9 button matrix with 
 //         *debounce
@@ -6,14 +7,27 @@
 //               - not sure if this is truly necessary, but on a faster chip with higher polling speed it migh make a difference 
 //         *button cooldown 
 //               - prevents a button press from being registered multiple times
-//               - disabled by setting "cooldownt" to 0
+//               - Independent values for each button, disabled by setting "cooldownt" to 0
 //               - when you disable the cooldown timer, button presses are still only registered once assuming it passes debounce
 //         *longhold 
 //               - you know when you hold a button for more than a second it just acts like continuous presses. that thing.
 //               - adjusted via "longholdt" 
-//               - Can't really be disabled, but you can set the number really high and that would effectively do the trick.  
+//               - Independent values for each button
+//         *Media Keys
+//               -added functions for easy mapping of media keys
+//         *****fully rebindable*****
+//              - each key is read as their address, that address is then mapped to a case in the keymap function, 
+//                which essentially executes as a script. For a full range of what can be put in these scripts, documentation  
+//                from PJRC (the teensyduino libs) and Arduino regarding keyboard communications and the signals that can be sent from the teensy
+//              
+//         ***** extensible *****
+//                - many parts will have to be manually adjusted to account, but I have tried to make it as painless as possible to
+//                  make your own matrix with however many rows and columns as you might want.
 //
 //          note: all time is counted in ms, or 1/1000 of a second
+//
+//
+//
 //
 //    You can map the keys however you wish, 
 //    but this is how it is configured by default
@@ -30,8 +44,8 @@
 //             1 | 3  | 4  | 5  |
 //               |----|----|----|
 //             2 | 6  | 7  | 8  |
-//                |----|----|----|
-    
+//               |----|----|----|
+
 
 // PIN ASSIGNMENT 
 //number of rows
@@ -47,7 +61,6 @@ const int buttons PROGMEM = rows * columns;
 const int col[columns] PROGMEM = {1, 2, 3};
 const int row[rows] PROGMEM = {9, 10, 11};
 
-
 //arrays for button debouncing
 int button_buff[buttons];
 int button_lastState[buttons];
@@ -60,16 +73,116 @@ LCqueue *outputBuffer;
 
 //cooldown timer, in case you want to prevent a button from being able to be pressed consecutively too quickly.
 unsigned long cooldown_timer[buttons];
-const unsigned long cooldownt PROGMEM = 100;
+
+//adjust depending upon the number of buttons in the matrix
+const unsigned long cooldownt[] PROGMEM = {
+            0,  //0
+            0,  //1
+            0,  //2
+            0,  //3
+            0,  //4
+            0,  //5
+            0,  //6
+            0,  //7
+            0   //8
+//Needs to be extended if more columns or rows are added
+        };
 
 //longhold timer, to prevent unnecessary numbers of consecutive registered button operations.  
 unsigned long longhold_timer[buttons]; //timer for longhold state limits how quickly longhold triggers a registered activation 
-const unsigned long longholdt PROGMEM = 1000;//time to longhold state
-const unsigned long longhold_active_t PROGMEM = 200;//limits how quickly longhold signals an output
+
+//adjust depending upon the number of buttons in the matrix
+const unsigned long longholdt[] PROGMEM = {//time to longhold state
+            0,  //0
+            0,  //1
+            0,  //2
+            0,  //3
+            0,  //4
+            0,  //5
+            0,  //6
+            0,  //7
+            0   //8
+//Needs to be extended if more columns or rows are added
+        };
+
+//controls how frequently the long hold operation registers an activation
+const unsigned long longhold_active_t PROGMEM = 200;
 
 //given col and row, returns button address and "number"
 int buttonaddr(int rownum, int colnum){
   return (rows * rownum) + (colnum);
+  }
+
+
+//The media codes all require the press and release function calls in order to register
+//this simply makes it easier for use in the keymap function 
+void mediaFunc(int A){ //
+    switch(A){
+      case KEY_MEDIA_PREV_TRACK :
+          Keyboard.press(KEY_MEDIA_PREV_TRACK);
+          Keyboard.release(KEY_MEDIA_PREV_TRACK);
+          break;
+      case KEY_MEDIA_PLAY_PAUSE :
+          Keyboard.press(KEY_MEDIA_PLAY_PAUSE);
+          Keyboard.release(KEY_MEDIA_PLAY_PAUSE);
+          break;
+      case KEY_MEDIA_NEXT_TRACK :
+          Keyboard.press(KEY_MEDIA_NEXT_TRACK);
+          Keyboard.release(KEY_MEDIA_NEXT_TRACK);
+          break;
+      case KEY_MEDIA_VOLUME_DEC :
+          Keyboard.press(KEY_MEDIA_VOLUME_DEC);
+          Keyboard.release(KEY_MEDIA_VOLUME_DEC);
+          break;
+      case KEY_MEDIA_VOLUME_INC : 
+          Keyboard.press(KEY_MEDIA_VOLUME_INC);
+          Keyboard.release(KEY_MEDIA_VOLUME_INC);
+          break;
+      case KEY_MEDIA_MUTE : 
+          Keyboard.press(KEY_MEDIA_MUTE);
+          Keyboard.release(KEY_MEDIA_MUTE);
+          break;
+    }
+  }
+
+//DEFAULT CONFIGURATION
+//function that handles mapping button input to desired keypad output. 
+//each case corresponds to the button number assignment
+void KeyMap(int A){
+    switch(A){
+      case 0 : // Previous Track
+          mediaFunc(KEY_MEDIA_PREV_TRACK);
+        break;
+      case 1 : // Play/Pause
+          mediaFunc(KEY_MEDIA_PLAY_PAUSE);
+        break;
+      case 2 : // Next Track
+          mediaFunc(KEY_MEDIA_NEXT_TRACK);
+        break;
+      case 3 : // Volume Down
+          mediaFunc(KEY_MEDIA_VOLUME_DEC);
+        break;
+      case 4 : // Mute
+          mediaFunc(KEY_MEDIA_MUTE);
+        break;
+      case 5 : // Volume Up
+          mediaFunc(KEY_MEDIA_VOLUME_INC);
+        break;
+      case 6 : //joystick 1
+          Joystick.button(1, 1);      
+          Joystick.button(1, 0);
+        break;
+      case 7 : //joystick 2
+          Joystick.button(2, 1);      
+          Joystick.button(2, 0);
+        break;
+      case 8 : //joystick 3
+          Joystick.button(3, 1);      
+          Joystick.button(3, 0);
+        break;
+      default :
+      break;
+      }
   }
 
 //debouncing pin reader, prevents rapid on/off switching or registering multiple button presses due to switch noise. 
@@ -98,9 +211,9 @@ bool checkLongHold(unsigned long button_cooldown_time,  unsigned long button_con
       *  after the state change reset because of the order of the flow. this next part is bronken up to be 
       *  easier to explain and read.   
       */
-     if(button_cooldown_time >= button_confirmed_state_change_time){
+     if(button_cooldown_time >= button_confirmed_state_change_time && longholdt[address] != 0){
       //if the button has been held down long enough for a long hold mode activation
-      if((millis() - button_cooldown_time) > longholdt){
+      if((millis() - button_cooldown_time) > longholdt[address]){
           //this is another timer checker used to limit how fast the longhold state registers activations
           if((millis() - longhold_timer[address]) > longhold_active_t){
           longhold_timer[address] = millis();
@@ -119,7 +232,7 @@ bool checkCoolDown(int address){//returns true if cooldown timer is exceeded, or
     if(checkLongHold(button_cooldown_time, button_confirmed_state_change_time, address)){
       return true;
       }
-    else if((millis() - button_cooldown_time) > cooldownt && button_confirmed_state_change_time > button_cooldown_time && cooldownt != 0){    //if cooldown timer is exceeded, resets timer, returns true //&& button_confirmed_state_change_time > button_cooldown_time 
+    else if((millis() - button_cooldown_time) > cooldownt[address] && button_confirmed_state_change_time > button_cooldown_time && cooldownt[address] != 0){    //if cooldown timer is exceeded, resets timer, returns true //&& button_confirmed_state_change_time > button_cooldown_time 
       cooldown_timer[address] = millis();
       return true;
     }                             
@@ -177,7 +290,7 @@ void genOutputBuffer(){
 void executeBuffer(){
     int action = pop(outputBuffer);
     if(action != -1){
-    Keyboard.print(action);
+    KeyMap(action);
     }
   }
   
